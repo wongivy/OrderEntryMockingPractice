a1f1fb334cd7b3da1b7b29a20a01d17ed827dcab
Chris McKenzie <crmckenzie@gmail.com>
2014-02-03 15:01:45 -0800

    Update Order.cs
    Add constructor to initialize collection property

--- OrderEntryMockingPractice/Models/Order.cs
+++ OrderEntryMockingPractice/Models/Order.cs
@@ -4,6 +4,11 @@ namespace OrderEntryMockingPractice.Models
 {
     public class Order
     {
+        public Order()
+        {
+            this.OrderItems = new List<OrderItem>();
+        }
+        
         public int? CustomerId { get; set; }
         public List<OrderItem> OrderItems { get; set; }
     }
