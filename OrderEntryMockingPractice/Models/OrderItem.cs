06c23ae78b0c83abe507a63754959bfac06a3877
Chris McKenzie <crmckenzie@gmail.com>
2014-02-04 10:26:42 -0800

    Update OrderItem.cs
    Changed datatype of OrderItem.Quantity

--- OrderEntryMockingPractice/Models/OrderItem.cs
+++ OrderEntryMockingPractice/Models/OrderItem.cs
@@ -3,6 +3,6 @@
     public class OrderItem
     {
         public Product Product { get; set; }
-        public double Quantity { get; set; }
+        public decimal Quantity { get; set; }
     }
 }
